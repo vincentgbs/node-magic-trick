984f9032cc9aef072102a9b46a4605675a13bcfa
vincent <vincent.gbs11@gmail.com>
2019-05-21 21:08:43 -0400

    Adding modules,es, app , compiling, still debugging routing

--- setup.sh
+++ setup.sh
@@ -1,11 +1,10 @@
 sudo apt update
 sudo apt install nodejs
 sudo apt install npm
-npm install -g express
-sudo npm install -g express
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
 sudo apt update
+# sudo apt install -y mongodb-org=4.0.9
 sudo apt install -y mongodb-org
 echo "mongodb-org hold" | sudo dpkg --set-selections
 echo "mongodb-org-server hold" | sudo dpkg --set-selections
@@ -13,3 +12,6 @@ echo "mongodb-org-shell hold" | sudo dpkg --set-selections
 echo "mongodb-org-mongos hold" | sudo dpkg --set-selections
 echo "mongodb-org-tools hold" | sudo dpkg --set-selections
 sudo service mongod start
+
+npm init
+npm install express body-parser mongodb
